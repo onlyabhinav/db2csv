d13ffcdb679673b36a7090b1b042a6909234bbe2
abhinav <abhinav.2310@gmail.com>
2023-06-11 12:55:25 +0530

    1st commit

---
+++ src/main/java/com/onlyabhinav/dbtextract/DbtextractApplication.java
@@ -0,0 +1,13 @@
+package com.onlyabhinav.dbtextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbtextractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbtextractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlyabhinav/dbtextract/DbtextractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyabhinav.dbtextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtextractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
